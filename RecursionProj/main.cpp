cfcb3e4082e84383b73d8a3dd33e0ac49a9ca044
Angela Rivas <snowcats001@gmail.com>
2019-03-06 13:01:50 -0700

    Used double value to calculate factorials in for loop

--- RecursionProj/main.cpp
+++ RecursionProj/main.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 using namespace std;
 
-int fib(int number)
+int fib(long number)
 {
     if(number == 0 || number == 1)
     {
@@ -20,13 +20,25 @@ int fib(int number)
         return fib(number-1) + fib(number -2);
     }
 }
+double fact (int numb)
+{
+    if (numb ==1 || numb == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return numb * fact(numb - 1);
+    }
+}
 int main()
 {
     // insert code here...
     int number = 10;
-    for(int index = 0; index < 20; index++)
+    for(int index = 0; index < 50; index++)
     {
-    cout<< "The Fibonacci sequence at" << number << "is: " << fib(number) << endl;
+//    cout<< "The Fibonacci sequence at" << number << "is: " << fib(number) << endl;
+    cout<< "The Factorial sequence at" << number << "is: " << fib(number) << endl;
     }
     return 0;
 }
